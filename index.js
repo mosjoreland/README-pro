692cfcb631bc8b5334de6529a3f29a83d0aa46f8
mosjoreland <jkw.42699@gmail.com>
2023-04-13 23:12:03 -0700

    added comments for reminders. added gen markdown file. added start up code from the class repo.

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+// TODO: Create an array of questions for user input
+// const questions = [];
+
+// TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+// function init() {}
+
+// Function call to initialize app
+// init();
 
 inquirer
   .prompt([
@@ -34,11 +47,13 @@ inquirer
       message: 'List any collaborators for this project, and provide their github profile links. Also include any third-party assets that require attribution, along with links to their primary web presence. Finally, include a link to any tutorials you may have followed, if any.'
     },
     {
+      // change to list
       type: 'input',
       name: 'License',
       message: 'Please provide the license used to create this project, to let other developers know what they can or can not do with your project.'
     },
     {
+      // get rid of this line and add badge when choice is selected
       type: 'input',
       name: 'Badges',
       message: 'Provide a badge of your choice to be displayed on your README. Please use the following format ![badge](https url)'
@@ -58,10 +73,11 @@ inquirer
       name: 'Tests',
       message: 'Go the extra mile and write tests for your application:'
     },
+    // add a prompt for user to include github username to be put in the questions section
   ])
   .then((answers) => {
     console.log(answers);
-
+    // change this to a text file while testing so you can write your own README lol
     fs.writeFile('README.md', JSON.stringify(answers, null, 2), (err) => {
       if (err) throw new Error();
 
